ac0c160dc0770b67b90339b88b8fb3820a3967f9
matthias <mpdev@pogodda.net>
2024-05-19 14:42:43 +0200

    removed some echos

--- config/run.sh
+++ config/run.sh
@@ -4,17 +4,17 @@ echo "Starting run.sh"
 cat /var/www/html/config/crontab.default > /var/www/html/config/crontab
 
 if [[ ${CRONJOB_SCHEDULE} && ${CRONJOB_SCHEDULE-x} ]]; then
-    echo "using cron schedule " ${CRONJOB_SCHEDULE}
+    echo "using cron schedule"
     sed -i -e "s;0 \* \* \* \*;${CRONJOB_SCHEDULE};g" /var/www/html/config/crontab
 elif [[ ${CRONJOB_ITERATION} && ${CRONJOB_ITERATION-x} ]]; then
-    echo "using cron iteration " ${CRONJOB_ITERATION}
-    sed -i -e "s;0;0/${CRONJOB_ITERATION};g" /var/www/html/config/crontab
+    echo "using cron iteration"
+    sed -i -e "s/0/*\/${CRONJOB_ITERATION}/g" /var/www/html/config/crontab
 fi
 cat /var/www/html/config/crontab
 crontab /var/www/html/config/crontab
 
 echo "Starting Cronjob"
-crond &
+crond -l 2 -f &
 
 echo "Starting nginx"
 exec nginx -g "daemon off;"
