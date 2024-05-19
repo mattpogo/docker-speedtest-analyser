825697c44d0be99a06c3368a50ad6a40904b66f9
matthias <mpdev@pogodda.net>
2024-05-19 14:18:19 +0200

    changed to accept full cron schedule in env variable CRONJOB_SCHEDULE

--- config/run.sh
+++ config/run.sh
@@ -5,15 +5,16 @@ cat /var/www/html/config/crontab.default > /var/www/html/config/crontab
 
 if [[ ${CRONJOB_SCHEDULE} && ${CRONJOB_SCHEDULE-x} ]]; then
     echo "using cron schedule " ${CRONJOB_SCHEDULE}
-    sed -i "s;0 \* \* \* \*;${CRONJOB_SCHEDULE};g" /var/www/html/config/crontab
+    sed -i -e "s;0 \* \* \* \*;${CRONJOB_SCHEDULE};g" /var/www/html/config/crontab
 elif [[ ${CRONJOB_ITERATION} && ${CRONJOB_ITERATION-x} ]]; then
     echo "using cron iteration " ${CRONJOB_ITERATION}
-    sed -i "s;0;*/${CRONJOB_ITERATION};g" /var/www/html/config/crontab
+    sed -i -e "s;0;0/${CRONJOB_ITERATION};g" /var/www/html/config/crontab
 fi
+cat /var/www/html/config/crontab
 crontab /var/www/html/config/crontab
 
 echo "Starting Cronjob"
-crond -l 2 -f &
+crond &
 
 echo "Starting nginx"
 exec nginx -g "daemon off;"
